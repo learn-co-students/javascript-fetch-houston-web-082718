0a101be370b09b73d1bd931d489f5fae389369bc
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-14 14:03:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much.";
+// const token = 'my secret token'
+fetch('https://api.github.com/users/sparkbold-git/repos'
+// , {
+//   headers: {
+//     Authorization: `token ${token}`
+//   }
+// }
+).then(res => res.json()).then(json => console.log(json));
+
